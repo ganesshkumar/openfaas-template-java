8162c350ea84b537ed54cc3f8c9e89dd4440a1e9
ganesshkumar <rpganesshkumar@gmail.com>
2017-12-18 20:40:02 +0530

    1. Correcting the java command in Dockerfile 2. Modifying Response.java status field from int to Stringt

--- template/java-gradle/com/openfaas/afterburn/parser/Parser.java
+++ template/java-gradle/com/openfaas/afterburn/parser/Parser.java
@@ -12,7 +12,7 @@ public class Parser {
     public void acceptIncoming(DataInputStream inputStream, BufferedWriter outputWriter) throws IOException {
         Request request = new Reader(inputStream).getRequest();
         String responseBody = handler.function(request.getBody(), request.getHeaders().getMethod());
-        String response = new Response(200, responseBody, "text/plain").serialize();
+        String response = new Response("200 OK", responseBody, "text/plain").serialize();
 
         outputWriter.write(response, 0, response.length());
         outputWriter.flush();
--- template/java-gradle/com/openfaas/afterburn/parser/Response.java
+++ template/java-gradle/com/openfaas/afterburn/parser/Response.java
@@ -3,11 +3,11 @@ package com.openfaas.afterburn.parser;
 public class Response {
     private static final String SEPARATOR = "/r/n";
 
-    private int status;
+    private String status;
     private String body;
     private String contentType;
 
-    public Response(int status, String body, String contentType) {
+    public Response(String status, String body, String contentType) {
         this.status = status;
         this.body = body;
         this.contentType = contentType;
@@ -23,7 +23,7 @@ public class Response {
 
     private String headers() {
         return new StringBuffer()
-                .append(String.format("HTTP/1.1 %s%s", status == 200 ? "200 OK" : String.valueOf(status), SEPARATOR))
+                .append(String.format("HTTP/1.1 %s%s", status, SEPARATOR))
                 .append(String.format("Content-Length: %s%s", body.length(), SEPARATOR))
                 .append(contentType != null ? String.format("Content-Type: %s%s", contentType, SEPARATOR): "")
                 .append(String.format("Connection: Close%s", SEPARATOR))
